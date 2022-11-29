29b721b4d72a07f98a2b0e3e9369df00e8af5eaa
James-Garofolo <garofo24@students.rowan.edu>
2022-11-28 20:27:15 -0500

    changed rules gen network

--- rules_network.py
+++ rules_network.py
@@ -1,3 +1,4 @@
+from torch.nn.modules.activation import LogSoftmax
 import numpy as np
 import torch
 from torch import nn
@@ -7,37 +8,38 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 from sklearn.model_selection import train_test_split
 from torch.nn import functional as func
-from add_unknowns import add_unknowns
 import os
+import math
+from add_unknowns import add_unknowns
 
-class one_tile_fc(nn.Module):
-    def __init__(self, columns, rows, tiles) -> None:
-        super().__init__()
-        
-        in_size = ((columns*rows)-1)*(tiles+1)
-        self.top = nn.Sequential(
-            nn.Linear(in_size, int(in_size*.75)),
-            nn.ReLU(),
-            nn.Linear(int(in_size*.75), tiles)
-        )
+"""
+change fc network guy to use embeddings with index inputs rather than one-hot vector inputs
+"""
 
-    def forward(self, x):
-        outs = self.top(outs)
-        return outs
 
 class whole_map_fc(nn.Module):
-    def __init__(self, columns, rows, tiles) -> None:
+    def __init__(self, columns, rows, tiles, hidden_ratio=1.25) -> None:
         super().__init__()
 
         self.columns = columns
         self.rows = rows
         self.tiles = tiles
-        self.flatten = nn.Flatten(1,-2) # flatten into array of tile vectors, preserve batch index
-        nets = []
-        for a in range(columns):
-            nets.append([])
-            for b in range(rows):
-                nets[a].append(one_tile_fc(columns, rows, tiles))
+        #print('saved dims')
+        embedding_dim = int(self.tiles/10)
+        print("embedding dims:", embedding_dim)
+        self.embed = nn.Embedding(self.tiles+1, embedding_dim)
+        self.flatten = nn.Flatten() # flatten into array of tile vectors, preserve batch index
+        #print('made special layers')
+        
+        self.top = nn.Sequential(
+            nn.Linear(columns*rows*embedding_dim, int(columns*rows*embedding_dim*hidden_ratio)),
+            nn.ReLU(),
+            nn.Linear(int(columns*rows*embedding_dim*hidden_ratio), columns*rows*(tiles-1)),
+            nn.ReLU()
+        )
+
+        self.softmax = nn.Softmax(3)
+        self.logify = nn.Sigmoid()
 
         
     def forward(self, maps):
@@ -47,41 +49,19 @@ class whole_map_fc(nn.Module):
         if not torch.is_tensor(maps):
             maps  = torch.tensor(maps)
 
+        #print("in shape:", maps.shape, torch.max(maps))
+        maps = self.embed(maps)
+        #print("embedded:", maps.shape)
         maps = self.flatten(maps)
+        #print("flattened:", maps.shape)
+        out_probs = self.top(maps)
+        #print("inferred", out_probs.shape)
+        #outs = self.softmax(out_probs.view(-1,self.columns,self.rows,self.tiles-1))
+        outs = self.logify(out_probs.view(-1,self.columns,self.rows,self.tiles-1))
+        #print("softmaxed", outs.shape, torch.max(outs))
 
-        out_probs = torch.empty((self.columns, self.rows, self.tiles))
 
-        
-        for a in self.columns:
-            offset = a*self.columns
-            for b in self.rows:
-                if self.training: # if in training, evaluate every tile
-                    # remove tile info for the tile the network is inferencing for
-                    in_maps = torch.cat((maps[:,:offset+b], maps[:,offset+b+1:]),1) 
-                    out_probs[:,a,b] = self.nets[a][b](in_maps)
-
-                else: # if in testing, don't evaluate already filled in tiles
-                    map_ids = []
-                    in_maps = False
-                    for c, m in enumerate(maps):
-                        if m[offset+b,-1]: # if this tile is unknown
-                            map_ids.append(c) # save the index
-                            in_map = torch.cat((m[:offset+b], m[offset+b+1:])) # make the map vector
-                            # add it to the array of vectors to infer on
-                            if in_maps:
-                                in_maps = torch.cat((in_maps,torch.reshape(in_map, [1,*list(in_map.shape)])))
-                            else:
-                                in_maps = torch.reshape(in_map, [1,*list(in_map.shape)])
-                        
-                        else: # if the tile is unknown, just return it as is
-                            out_probs[c,a,b] = m[offset+b]
-                            
-                    out_calc = self.nets[a][b](in_maps) # infer on all of the ones we need to
-
-                    for c, m in enumerate(out_calc): # add the inferences back into the fold
-                        out_probs[map_ids[c],a,b] = out_calc[c]
-
-        return out_probs
+        return outs
 
 
 
@@ -107,14 +87,31 @@ def get_data(path: str):
     return maps, length
 
 
-def train(data, labels, model, device, loss_fn, optimizer, batch_size=64):
+def idx_to_one_hot(ids: np.array, max_id):
+    outs = np.zeros((ids.shape[0], ids.shape[1], ids.shape[2], max_id), dtype=np.single)
+    for a, map in enumerate(ids):
+        for b, row in enumerate(map):
+            for c, id in enumerate(row):
+                outs[a, b, c, id] = 1.00
+
+    return outs
+
+
+
+def train(data, labels, model, device, loss_fn, optimizer, verbose=True, batch_size=64):
     model.train()
 
     avg_loss = 0
     counter = 0
+    correct = 0
+    classified = 0
 
     size = data.shape[0]
-    batches = np.ceil(size/batch_size)
+    batches = int(np.ceil(size/batch_size))
+
+    if not torch.is_tensor(data):
+        data = torch.from_numpy(data)
+        labels = torch.from_numpy(labels)
 
     for batch in range(batches):
         try:
@@ -127,8 +124,13 @@ def train(data, labels, model, device, loss_fn, optimizer, batch_size=64):
 
         # Compute prediction error
         pred = model(X)
+        
         loss = loss_fn(pred, y)
-
+        tiles = torch.argmax(pred, dim=3)
+        label_tiles = torch.argmax(y, dim=3)
+        
+        correct += (tiles==label_tiles).type(torch.float).sum().item()
+        classified += torch.numel(tiles)
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
@@ -136,15 +138,22 @@ def train(data, labels, model, device, loss_fn, optimizer, batch_size=64):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            if verbose:
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+                print(f"acc: {correct/classified}")
             avg_loss += loss
             counter += 1
 
-    return avg_loss/counter
+    return correct/classified, avg_loss/counter
 
 
 def test(data, labels, model, device, loss_fn):
-    
+    correct = 0
+    classified = 0
+    if not torch.is_tensor(data):
+        data = torch.from_numpy(data)
+        labels = torch.from_numpy(labels)
+
     model.eval()
     size = data.shape[0]
     with torch.no_grad():
@@ -153,40 +162,51 @@ def test(data, labels, model, device, loss_fn):
         X, y = X.to(device), y.to(device)
         pred = model(X)
         test_loss = loss_fn(pred, y).item()
-        correct = (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    return 100*correct, loss
+        test_loss /= size
+        
+        tiles = torch.argmax(pred, dim=3)
+        label_tiles = torch.argmax(y, dim=3)
+        
+        correct += (tiles==label_tiles).type(torch.float).sum().item()
+        classified += torch.numel(tiles)
+    print(f"Avg loss: {test_loss:>8f} \n")
+    print(f"Avg acc: {correct/classified}\n")
+    return correct/classified, test_loss
 
 
 if __name__ == "__main__":
     full_windows, max_id = get_data(os.getcwd() + "/data/map_vectors/numpy/")
     print("data in: ", full_windows.shape)
-    data_windows, label_windows = add_unknowns(full_windows, 20, max_id)
+    data_windows, label_windows = add_unknowns(full_windows, 100, max_id)
     print("with unknowns added: ", data_windows.shape, label_windows.shape)
+    print(max_id)
 
     train_data, val_data, train_labels, val_labels = train_test_split(data_windows, label_windows, test_size=0.1)
     
+    train_labels = idx_to_one_hot(train_labels, max_id)
+    val_labels = idx_to_one_hot(val_labels, max_id)
+    
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
 
-    model = whole_map_fc(label_windows.shape[1], label_windows.shape[2], label_windows.shape[3])
-    loss = nn.CrossEntropyLoss()
-    optim = torch.optim.Adam(lr=0.001, weight_decay=1e-5)
+    model = whole_map_fc(label_windows.shape[1], label_windows.shape[2], max_id+1, 0.5)
+    loss = nn.BCELoss()
+    optim = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
+    print('starting to train')
 
     best_loss = None
     best_acc = 0
-    epochs = 20
+    epochs = 50
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        train_loss = train(train_data, train_labels, model, device, loss, optim)
+        train_acc, train_loss = train(train_data, train_labels, model, device, loss, optim, False)
         test_acc, test_loss = test(val_data, val_labels, model, device, loss)
-        if (best_loss == None) or (best_loss > test_loss) or (best_acc < test_acc):
-            best_acc = test_acc
+        if (best_loss == None) or (best_loss > test_loss) or (test_acc > best_acc):
             best_loss = test_loss
-            with open('rules_gen_fc.pt', 'wb') as f:
+            best_acc = test_acc
+            with open('rules_gen_fc_long.pt', 'wb') as f:
                 torch.save(model, f)
         
         else:
