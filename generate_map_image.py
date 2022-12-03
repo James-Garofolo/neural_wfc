7031be1dd9b5ffa6e8714f0527f4df70a548c57b
JL102 <jordanlees@mailbox.org>
2022-12-02 20:16:00 -0500

    Got a "working" version of wave function collapse

---
+++ generate_map_image.py
@@ -0,0 +1,54 @@
+import os, sys, pathlib, math
+from types import FunctionType
+from PIL import Image, ImageDraw
+import numpy as np
+from hex_onehot import ONEHOT_LENGTH, onehot_to_hex
+from split_map import DIR_TILES_OUTPUT, DIR_DATA, DIR_MAPVECTORS_NP_OUTPUT, TILE_SIZE
+
+tile_sprite_dict = {}
+
+def generate_image(map: np.ndarray, path: str, is_onehot):
+	img = Image.new(mode = 'RGB',
+		size = (map.shape[0] * TILE_SIZE, map.shape[1] * TILE_SIZE),
+		color = (255, 0, 255) # Default to an ugly magenta so we can see if there are any tiles that failed to paste
+	)
+	for x in range(len(map)):
+		column = map[x]
+		for y in range(len(column)):
+			
+			is_unknown_tile = False
+			
+			if is_onehot:
+				# Convert onehot to hexadecimal
+				onehot = column[y]
+				tile_name = onehot_to_hex(onehot)
+			else:
+				# Convert index (as int) to hexadecimal string
+				tile_name = f'{column[y]:02x}'
+				# If the index of the tile is outside the maximum index, that means it's an unknown tile
+				is_unknown_tile = (column[y] >= ONEHOT_LENGTH) 
+			
+			# Skip unknown tiles
+			if not is_unknown_tile:
+				# If we don't have the sprite in our tile_sprite_dict, load it from the image file
+				if (not tile_name in tile_sprite_dict):
+					with Image.open(os.path.join(DIR_TILES_OUTPUT, f'{tile_name}.png')) as tile_sprite:
+						tile_sprite_dict[tile_name] = tile_sprite.copy()
+				coord_x = x * TILE_SIZE
+				coord_y = y * TILE_SIZE
+				img.paste(tile_sprite_dict[tile_name], (coord_x, coord_y))
+	img.save(path, 'PNG')
+	
+# Generate map image from onehot ndarray
+def from_onehot(map_onehot: np.ndarray, path: str):
+	return generate_image(map_onehot, path, True)
+
+# Generate map image from ndarray of int indexes
+def from_indexes(map_indexes: np.ndarray, path: str):
+	return generate_image(map_indexes, path, False)
+
+if __name__ == "__main__":
+	
+	map_zero = np.load(os.path.join(DIR_MAPVECTORS_NP_OUTPUT, '0.npy'), allow_pickle=True)
+	
+	from_onehot(map_zero, os.path.join(DIR_DATA, 'map0.png'))
---
+++ wave_function_collapse.py
@@ -0,0 +1,131 @@
+import os, sys, pathlib, math
+from types import FunctionType
+from PIL import Image, ImageDraw
+import numpy as np
+import pandas as pd
+import torch
+from hex_onehot import ONEHOT_LENGTH, hex_to_onehot
+from rules_network import whole_map_fc, get_data
+from split_map import DIR_DATA, DIRNAME, DIR_MAPVECTORS_NP_OUTPUT
+import generate_map_image
+import random
+
+class wave_function_collapse:
+	
+	"""
+	
+	inputs:
+		map_shape: sdfdsf
+		num_possible_tiles: sdfsdf
+	"""
+	def __init__(self, map_shape, num_possible_tiles) -> None:
+		self.manual_rules = []
+		self.undefined_tile = num_possible_tiles # update later
+		self.possibilities = np.zeros((map_shape[0], map_shape[1], num_possible_tiles))
+		self.collapsed_tiles = np.ones(map_shape, dtype=int) * (self.undefined_tile)
+	
+	"""
+	Returns the first state of the board
+	"""
+	def first_step(self) -> np.ndarray:
+		return self.collapsed_tiles
+	
+	""" 
+	Calculates and returns the next state of the board
+	
+	"""
+	def step(self, new_possibilities: np.ndarray) -> np.ndarray:
+		self.possibilities = new_possibilities
+		
+		lowest_entropy_coords = self.find_lowest_entropy()
+		if len(lowest_entropy_coords) == 0:
+			print('Done')
+			return self.collapsed_tiles
+		
+		self.collapse_state(lowest_entropy_coords)
+		
+		return self.collapsed_tiles
+	
+	"""
+	Finds the tile(s) with the lowest entropy
+	Returns the x and y indices of those tiles
+	"""
+	def find_lowest_entropy(self):
+		lowest_entropy = 9999999
+		lowest_coords = []
+		for y in range(len(self.possibilities)):
+			column: np.ndarray = self.possibilities[y]
+			for x in range(len(column)):
+				this_multihot: np.ndarray = column[x]
+				# Find the indices where this multihot is 1 / true
+				where = np.where(this_multihot == 1)[0]
+				this_entropy = len(where)
+				# if this is the new lowest, reset lowest_coords
+				if (this_entropy > 0) and (this_entropy < lowest_entropy):
+					lowest_coords = [(y, x)]
+					lowest_entropy = this_entropy
+				# if this has the same entropy as lowest, simply append
+				elif this_entropy == lowest_entropy:
+					lowest_coords.append((y, x))
+		if lowest_entropy > 1:
+			print(f'Lowest entropy is {lowest_entropy}')
+		return lowest_coords
+	
+	"""
+	Collapse the state of a given tile, or randomly choose between 
+	multiple tiles and collapse the state of that tile
+	"""
+	def collapse_state(self, coords_to_collapse):
+		for coords in coords_to_collapse:
+			multihot_to_collapse = self.possibilities[coords]
+			where = np.where(multihot_to_collapse == 1)[0]
+			# Collapse to a random index within the list of indices that == 1
+			# print(self.possibilities[coords_to_collapse])
+			
+			collapsed_index = where[random.randint(0, len(where) - 1)]
+			# Set collapsed_tiles to the new tile index at these coords
+			self.collapsed_tiles[coords] = collapsed_index
+			# print(f'Collapsed tile {coords} into {collapsed_index:02x}')
+		# if len(coords) == 1:
+		# 	coords_to_collapse = coords[0]
+		# else:
+		# 	# Randomly choose a tile to collapse
+		# 	index = random.randint(0, len(coords) - 1)
+		# 	coords_to_collapse = coords[index]
+	
+	def add_manual_rule(self, name: str, func: FunctionType):
+		pass
+
+
+if __name__ == '__main__':	
+	# Get the shape of the maps
+	map_zero = np.load(os.path.join(DIR_MAPVECTORS_NP_OUTPUT, '0.npy'), allow_pickle=True)
+	
+	wfc = wave_function_collapse(map_zero.shape, ONEHOT_LENGTH)
+	
+	# Load the PyTorch model
+	model_file = os.path.join(DIRNAME, 'rules_gen_fc_long.pt')
+	with open(model_file, 'rb') as f:
+		model: whole_map_fc = torch.load(f)
+	
+	model.to('cpu') # just run on cpu to keep things simpler
+	
+	# Get the initial board state
+	collapsed_tiles = wfc.first_step()
+	
+	for i in range(30):
+		print(f'\nSTEP {i}\n')
+		batch = torch.from_numpy(np.array([collapsed_tiles], dtype=int))
+		
+		# Make prediction
+		nn_prediction = model(batch)[0]
+		# Transform tensors that are > 0.5 into 1, and <= 0.5 into 0
+		nn_prediction = torch.threshold(nn_prediction, 0.5, 0)
+		nn_prediction = torch.ceil(nn_prediction)
+		tile_multihot = nn_prediction.detach().numpy()
+		
+		# Perform WFC computations
+		collapsed_tiles = wfc.step(tile_multihot)
+		
+		# Generate an image so we can visualize what the current map looks like
+		generate_map_image.from_indexes(collapsed_tiles, os.path.join(DIR_DATA, f'step_{i}.png'))
